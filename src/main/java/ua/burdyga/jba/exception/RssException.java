f567849b4166cfadb1a65edce5b6a5f33097a533
DimaSanKiev <dmytro.burdyga@gmail.com>
2015-06-16 03:13:32 +0300

    add unmarshalling XML file

---
+++ src/main/java/ua/burdyga/jba/exception/RssException.java
@@ -0,0 +1,8 @@
+package ua.burdyga.jba.exception;
+
+public class RssException extends Exception {
+
+    public RssException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/ua/burdyga/jba/service/RssService.java
@@ -0,0 +1,78 @@
+package ua.burdyga.jba.service;
+
+import org.springframework.stereotype.Service;
+import ua.burdyga.jba.entity.Item;
+import ua.burdyga.jba.exception.RssException;
+import ua.burdyga.jba.rss.ObjectFactory;
+import ua.burdyga.jba.rss.TRss;
+import ua.burdyga.jba.rss.TRssChannel;
+import ua.burdyga.jba.rss.TRssItem;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+@Service
+public class RssService {
+
+    public List<Item> getItems(Source source) throws RssException {
+        ArrayList<Item> list = new ArrayList<>();
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();                   // converts xml to java objects
+            JAXBElement<TRss> jaxbElement = unmarshaller.unmarshal(source, TRss.class);     // TRss.class is a result
+            TRss rss = jaxbElement.getValue();
+
+            List<TRssChannel> channels = rss.getChannel();
+            for (TRssChannel channel : channels) {
+                List<TRssItem> items = channel.getItem();
+                for (TRssItem rssItem : items) {
+                    Item item = new Item();
+                    item.setTitle(rssItem.getTitle());
+                    item.setDescription(rssItem.getDescription());
+                    item.setLink(rssItem.getLink());
+                    Date pubDate = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH).parse(rssItem.getPubDate());// Date pattern: Tue, 16 Jun 2015 03:06:49 +0000
+                    item.setPublishedDate(pubDate);
+                    list.add(item);
+                }
+            }
+        } catch (JAXBException e) {
+            throw new RssException(e);
+        } catch (ParseException e) {
+            throw new RssException(e);
+        }
+        return list;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
